d269a55cc29fdc2338563e04aaafc33b77051069
Arhab <asafwan72@gmail.com>
2021-01-01 03:05:41 +0600

    change

---
+++ HospitalManagement1/DataAccessLayer/CatagoryDataAccess.cs
@@ -0,0 +1,33 @@
+﻿using HospitalManagement1.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalManagement1.DataAccessLayer
+{
+    class CatagoryDataAccess
+    {
+        DataAccess dataAccess;
+        public CatagoryDataAccess()
+        {
+            this.dataAccess = new DataAccess();
+        }
+        public List<Staff> GetAllStaffs()
+        {
+            string sql = "SELECT FROM Staffs";
+            SqlDataReader reader =this.dataAccess.GetData(sql);
+            List<Staff> staffs = new List<Staff>();
+            while (reader.Read())
+            {
+                Staff staff = new Staff();
+                staff.StaffId = (int)reader["StaffId"];
+                staff.StaffName = reader["StaffName"].ToString();
+                staffs.Add(staff); 
+            }
+            return staffs;
+        }
+    }
+}
