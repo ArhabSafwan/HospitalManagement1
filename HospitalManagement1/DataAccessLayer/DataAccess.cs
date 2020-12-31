9eb8e7c46ac480ffd880e8fdee4838ee3edf90f1
Arhab <asafwan72@gmail.com>
2021-01-01 02:32:54 +0600

    change

---
+++ HospitalManagement1/DataAccessLayer/DataAccess.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalManagement1.DataAccessLayer
+{
+    class DataAccess
+    {
+        SqlConnection connection;
+        SqlCommand command;
+        public DataAccess()
+        {
+            this.connection = new SqlConnection(ConfigurationManager.ConnectionStrings["OP2"].ConnectionString);
+            this.connection.Open();
+
+        }
+    }
+}
