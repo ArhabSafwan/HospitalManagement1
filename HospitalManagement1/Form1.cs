c74370743ae2ba08fa00162f3a746df4fc323f37
Arhab <asafwan72@gmail.com>
2020-12-31 16:56:34 +0600

    change

--- HospitalManagement1/Form1.cs
+++ HospitalManagement1/Form1.cs
@@ -22,5 +22,10 @@ namespace HospitalManagement1
         {
 
         }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Application.Exit();
+        }
     }
 }
